d1c43ecdf8f780c90b94b9920efc4a799cfac3c5
吳姿萱 <cherry51120@gmail.com>
2018-04-03 14:56:06 +0800

    Initial Commit

---
+++ chestnutTests/chestnutTests.swift
@@ -0,0 +1,36 @@
+//
+//  chestnutTests.swift
+//  chestnutTests
+//
+//  Created by 吳姿萱 on 2018/4/3.
+//  Copyright © 2018年 吳姿萱. All rights reserved.
+//
+
+import XCTest
+@testable import chestnut
+
+class chestnutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
